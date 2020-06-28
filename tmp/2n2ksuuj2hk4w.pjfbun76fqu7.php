82d7cb66a12cc13d3b93e770be3d555dad6943c0
JagoKapok <g2a.ilhamubarok@gmail.com>
2020-06-28 22:44:43 +0700

    Remove tmp folder

--- tmp/2n2ksuuj2hk4w.pjfbun76fqu7.php
+++
@@ -1,3 +0,0 @@
-<?php echo $this->render($header,NULL,get_defined_vars(),0); ?>
-<?php echo $this->render($view,NULL,get_defined_vars(),0); ?>
-<?php echo $this->render('footer.html',NULL,get_defined_vars(),0); ?>
